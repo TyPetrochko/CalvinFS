4915041574b9bafec90f27035b20e7f9aed1b907
Kun Ren <kun.ren@yale.edu>
2016-12-14 00:47:55 +0000

    More debugging

--- src_remaster/fs/calvinfs_client_app.h
+++ src_remaster/fs/calvinfs_client_app.h
@@ -783,15 +783,20 @@ void LatencyExperimentAppend() {
     }
 
     uint64 size_other_machines = machines_other_replicas.size();
-
+    
+    LOG(ERROR) << "Init a";
     Spin(1);
+    LOG(ERROR) << "Init b";
     metadata_->Init();
+    LOG(ERROR) << "Init c";
     Spin(1);
+    LOG(ERROR) << "Init d";
     machine()->GlobalBarrier();
+    LOG(ERROR) << "Init e";
     Spin(1);
 
     double start = GetTime();
-    
+    LOG(ERROR) << "Starting up experiments!";
     for (int j = 0; j < 500; j++) {
       // Copy operations that cross data centers
        BackgroundRemasterFile(
