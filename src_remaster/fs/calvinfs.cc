44a2a44ad4df5bfa2a57df315f391fd28cfc4618
Lee Danilek <lee.danilek@yale.edu>
2016-12-21 02:24:20 -0500

    added a dest field to remaster input

--- src_remaster/fs/calvinfs.cc
+++ src_remaster/fs/calvinfs.cc
@@ -189,7 +189,7 @@ void CalvinFSConfigMap::SendIntrareplicaRemasterRequests(MetadataAction::Remaste
       a->set_action_type(wait ? MetadataAction::REMASTER_SYNC : MetadataAction::REMASTER_ASYNC);
       a->set_remaster(true);
       a->set_distinct_id(distinct_id);
-      a->set_dest(to_machine);
+      in.set_dest(to_machine);
 
       in.SerializeToString(a->mutable_input());
       a->add_readset(in.path());
@@ -270,12 +270,12 @@ void CalvinFSConfigMap::SendRemasterRequest(Machine* machine, uint32 to_machine,
   }
   a->set_remaster(true);
   a->set_distinct_id(distinct_id);
-  a->set_dest(to_machine);
 
   MetadataAction::RemasterInput in;
   in.set_path(path.data(), path.size());
   in.set_old_master(old_master);
   in.set_new_master(new_master);
+  in.set_dest(to_machine);
   in.SerializeToString(a->mutable_input());
   a->add_readset(path);
   a->add_writeset(path);
