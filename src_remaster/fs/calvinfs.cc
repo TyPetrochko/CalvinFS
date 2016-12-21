7bd40cd34c019c80fd9994a1880bc2f867bdda4d
Lee Danilek <lee.danilek@yale.edu>
2016-12-20 23:59:20 -0500

    .

--- src_remaster/fs/calvinfs.cc
+++ src_remaster/fs/calvinfs.cc
@@ -207,9 +207,11 @@ void CalvinFSConfigMap::SendIntrareplicaRemasterRequests(MetadataAction::Remaste
   }
 
   // now that all RPCs have been sent, wait for responses
+  LOG(ERROR) << "Begin waiting for synchronous responses";
   while (ack_counter.load() < to_expect) {
     usleep(10);
   }
+  LOG(ERROR) << "Done waiting for synchronous responses";
 }
 
 void CalvinFSConfigMap::SendRemasterFollows(MetadataAction::RemasterInput in, Machine* machine) {
